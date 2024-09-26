868dd0f58c3fb3c43ccffe55ae4f74d1e91d7deb
MaryJenn05 <u202120911@upc.edu.pe>
2024-09-25 23:06:43 -0500

    feat: set up packages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import mapboxgl from 'mapbox-gl';
+mapboxgl.accessToken = 'pk.eyJ1IjoibWplbm4iLCJhIjoiY20xZW5oNWZiMnI5djJrb2kzOWd3cWN4byJ9.KyJ5iCnNVesFZgzYoUG2og';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
 };
