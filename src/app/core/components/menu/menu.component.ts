7846dabb2f5e8c411becb1729b7f04a4bc2b1128
Cristian Quito <cristianandresquito@gmail.com>
2024-10-31 22:06:57 -0500

    feat: import modules

--- src/app/core/components/menu/menu.component.ts
+++ src/app/core/components/menu/menu.component.ts
@@ -1,21 +1,37 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { CustomSidenavComponent } from "../menu/components/custom-sidenav/custom-sidenav.component";
+import { MatMenuModule } from '@angular/material/menu';
+import { AuthService } from '@app/features/auth/services/auth.service';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule, RouterOutlet, CustomSidenavComponent],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule, RouterOutlet, CustomSidenavComponent,
+    MatMenuModule
+  ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+  private _router = inject(Router);
+  private _authService = inject(AuthService);
+
+  public username = computed(() => this._authService.currentUserName());
+
   collapse = signal<boolean>(false);
   sidenavWidth = computed(() => this.collapse() ? '0px' : '250px');
   showFiller = false;
 
+  goToDevices(): void {
+    this._router.navigate(['guardian-area/devices']);
+  }
+
+  goToProfile(): void {
+    this._router.navigate(['guardian-area/profile']);
+  }
 }
