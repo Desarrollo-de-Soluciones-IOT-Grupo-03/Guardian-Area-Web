4765d790e391a04d198ab59601eea131fcdfbbef
Diego Esquivel <u202116749@upc.edu.pe>
2024-10-31 21:59:54 -0500

    feat: implement sign-in functionality with username and error handling

--- src/app/features/auth/pages/sign-in/sign-in.component.ts
+++ src/app/features/auth/pages/sign-in/sign-in.component.ts
@@ -5,7 +5,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-sign-in',
   standalone: true,
@@ -14,10 +16,30 @@ import { RouterModule } from '@angular/router';
   styleUrl: './sign-in.component.css'
 })
 export class SignInComponent {
+  private _snackBar = inject(MatSnackBar);
   private _formBuilder = inject(FormBuilder);
+  private _authService = inject(AuthService);
+  private _router = inject(Router);
 
   form: FormGroup = this._formBuilder.group({
-    email: new FormControl<string>('', [Validators.required, Validators.email]),
+    username: new FormControl<string>('', [Validators.required]),
     password: new FormControl<string>('', Validators.required),
   });
+
+  login(): void {
+    this._authService.signIn(this.form.value).subscribe(
+      {
+        next: success => {
+          this._router.navigate(['/guardian-area/home']);
+        },
+        error: (error) => {
+          this._snackBar.open(error.error.message, 'Cerrar', {
+            duration: 3000,
+            horizontalPosition: 'right',
+            verticalPosition: 'top'
+          });
+        }
+      }
+    );
+  }
 }
