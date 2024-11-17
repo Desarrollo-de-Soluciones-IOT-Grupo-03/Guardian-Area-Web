dff64f40d2235dd2abe6d5d9a8e80d6edf714e2f
Cristian Quito <cristianandresquito@gmail.com>
2024-11-17 12:03:49 -0500

    feat: table activities components

--- src/app/features/activities/components/table-activities/table-activities.component.ts
+++ src/app/features/activities/components/table-activities/table-activities.component.ts
@@ -1,37 +1,42 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { Activity } from '../../models/activity';
-import { ActivityQuery } from '../../models/activity-query';
-import { ActivityService } from '../../services/activity.service';
-import { MatTableModule } from '@angular/material/table';
+import { Activity, ActivityQuery } from '@activities/models';
 import { CommonModule } from '@angular/common';
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { DeviceService } from '@devices/services';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-table-activities',
   standalone: true,
   imports: [MatTableModule, CommonModule],
   templateUrl: './table-activities.component.html',
-  styleUrl: './table-activities.component.css'
+  styleUrl: './table-activities.component.css',
 })
-export class TableActivitiesComponent implements OnInit{
+export class TableActivitiesComponent implements OnInit {
+  @Input() query?: ActivityQuery;
 
-  private _activityService: ActivityService = inject(ActivityService);
   private _snackBar = inject(MatSnackBar);
+  private _deviceService = inject(DeviceService);
 
-  displayedColumns: string[] = ['event', 'datetime', 'duration', 'actionTaken'];
+  displayedColumns: string[] = ['activityName', 'datetime', 'activityType'];
   activities: Activity[] = [];
+  showSpinner: boolean = false;
 
   ngOnInit(): void {
     this.getAllActivitiesByUserId();
   }
 
   getAllActivitiesByUserId(): void {
-    let query = { userId: 1 } as ActivityQuery;
-    this._activityService.getAllActivitiesByUserId(query).subscribe(
-      {
-        next: (activities: Activity[]) => this.activities = activities,
-      }
-    )
+    this.showSpinner = true;
+    const deviceRecordId = this._deviceService.deviceRecordId;
+    this._deviceService
+      .getAllByRecordId(deviceRecordId!, this.query ?? null)
+      .pipe(finalize(() => (this.showSpinner = false)))
+      .subscribe({
+        next: (activities: Activity[]) =>
+          (this.activities = activities.slice(-5)),
+      });
   }
 
   openSnackBar(message: string, action: string) {
