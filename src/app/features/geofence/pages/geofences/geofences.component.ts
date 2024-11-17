3a042e207b77f08e1badce7a5495797c6e9665c7
Diego Castro <diego_castro_soto@hotmail.com>
2024-11-17 13:43:58 -0500

    feat: add geofences page implementation

--- src/app/features/geofence/pages/geofences/geofences.component.ts
+++ src/app/features/geofence/pages/geofences/geofences.component.ts
@@ -1,34 +1,42 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
-import { Geofence } from '../../models/geofence';
-import { DeviceService } from '@app/features/devices/services/device.service';
-import { finalize } from 'rxjs';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MiniMapComponent } from "../../../maps/components/mini-map/mini-map.component";
-import { PreviewMapComponent } from "../../../maps/components/preview-map/preview-map.component";
 import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router, RouterLink } from '@angular/router';
+import { DeviceService } from '@devices/services';
+import { Geofence } from '@geofence/models';
+import { PreviewMapComponent } from '@maps/components';
+import { finalize } from 'rxjs';
+import { MatCardModule } from '@angular/material/card';
+
 @Component({
   selector: 'app-geofences',
   standalone: true,
-  imports: [MatIconModule, RouterLink, MatCardModule, MatButtonModule, MiniMapComponent, PreviewMapComponent,
-    CommonModule
+  imports: [
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,
+    PreviewMapComponent,
+    CommonModule,
+    RouterLink,
   ],
   templateUrl: './geofences.component.html',
-  styleUrl: './geofences.component.css'
+  styleUrl: './geofences.component.css',
 })
 export class GeofencesComponent implements OnInit {
   private _deviceService = inject(DeviceService);
   private _snackBar = inject(MatSnackBar);
+  private _router = inject(Router);
 
   geofences: Geofence[] = [];
   showSpinner: boolean = false;
+
   ngOnInit(): void {
     this.showSpinner = true;
-    this._deviceService.getAllGeofences(this._deviceService.deviceRecordId!)
-      .pipe(finalize(() => this.showSpinner = false))
+    this._deviceService
+      .getAllGeofences(this._deviceService.deviceRecordId!)
+      .pipe(finalize(() => (this.showSpinner = false)))
       .subscribe({
         next: (geofences) => {
           this.geofences = geofences;
@@ -37,11 +45,13 @@ export class GeofencesComponent implements OnInit {
           this._snackBar.open(error.error.message, 'Close', {
             duration: 3000,
             horizontalPosition: 'right',
-            verticalPosition: 'top'
+            verticalPosition: 'top',
           });
-        }
+        },
       });
-
   }
 
+  goToEditGeofence(id: string): void {
+    this._router.navigate(['geofences/edit', id]);
+  }
 }
