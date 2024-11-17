0af39a1ffc28b483d7afc2c6a942c5e57f49ee9c
Diego Esquivel <u202116749@upc.edu.pe>
2024-11-17 11:04:32 -0500

    feat: refactor main panel component to include sensor data retrieval

--- src/app/features/home/pages/main-panel/main-panel.component.ts
+++ src/app/features/home/pages/main-panel/main-panel.component.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
-import { MiniMapComponent } from '@app/features/maps/components/mini-map/mini-map.component';
-import { TableActivitiesComponent } from "../../../activities/components/table-activities/table-activities.component";
+import { TableActivitiesComponent } from '@activities/components';
+import { Component, inject, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { DeviceService, SocketService } from '@devices/services';
+import { MiniMapComponent } from '@maps/components';
 
 @Component({
   selector: 'app-main-panel',
   standalone: true,
   imports: [MiniMapComponent, TableActivitiesComponent],
   templateUrl: './main-panel.component.html',
-  styleUrl: './main-panel.component.css'
+  styleUrl: './main-panel.component.css',
 })
-export class MainPanelComponent {
+export class MainPanelComponent implements OnInit {
+  private _socketService = inject(SocketService);
+  private _deviceService = inject(DeviceService);
+  private _snackBar = inject(MatSnackBar);
 
+  bpm: number = 0;
+  spo2: number = 0;
 
+  ngOnInit(): void {
+    this._socketService.getSensorsData(this._deviceService.apiKey!).subscribe({
+      next: (data) => {
+        this.bpm = data.bpm;
+        this.spo2 = data.spo2;
+      },
+      error: (error) => {
+        this._snackBar.open('There are some problems', 'Close', {
+          duration: 3000,
+          horizontalPosition: 'right',
+          verticalPosition: 'top',
+        });
+      },
+    });
+  }
 }
