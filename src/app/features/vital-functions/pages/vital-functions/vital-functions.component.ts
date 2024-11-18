a323c488d711abf37845c96754a218f435493e0d
steve <stev8855@gmail.com>
2024-11-18 11:29:40 -0500

    feat: enhance vital functions component with data fetching and tab management

--- src/app/features/vital-functions/pages/vital-functions/vital-functions.component.ts
+++ src/app/features/vital-functions/pages/vital-functions/vital-functions.component.ts
@@ -1,15 +1,67 @@
-import { Component } from '@angular/core';
-import { HeartRateMonitoringTabComponent } from "../../components/heart-rate-monitoring-tab/heart-rate-monitoring-tab.component";
-import { OxygenationMonitoringTabComponent } from "../../components/oxygenation-monitoring-tab/oxygenation-monitoring-tab.component";
+import { Component, inject, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { DeviceService, SocketService } from '@devices/services';
+import {
+  HeartRateMonitoringTabComponent,
+  OxygenationMonitoringTabComponent,
+} from '@vital-functions/components';
+import { Chart } from 'chart.js';
+import { finalize } from 'rxjs';
 import { MatTabsModule } from '@angular/material/tabs';
+import { TableActivitiesComponent } from '@activities/components';
 
 @Component({
   selector: 'app-vital-functions',
   standalone: true,
-  imports: [HeartRateMonitoringTabComponent, OxygenationMonitoringTabComponent, MatTabsModule],
+  imports: [
+    HeartRateMonitoringTabComponent,
+    OxygenationMonitoringTabComponent,
+    MatTabsModule,
+    TableActivitiesComponent,
+  ],
   templateUrl: './vital-functions.component.html',
-  styleUrl: './vital-functions.component.css'
+  styleUrl: './vital-functions.component.css',
 })
-export class VitalFunctionsComponent {
+export class VitalFunctionsComponent implements OnInit {
+  private _snackBar = inject(MatSnackBar);
+  private _deviceService = inject(DeviceService);
 
+  id: string = 'heart-tab';
+  chart: Chart | null = null;
+  chart2: Chart | null = null;
+  showSpinner: boolean = false;
+  heartRateData: number[] = [];
+  oxygenSaturationData: number[] = [];
+  dates: string[] = [];
+
+  ngOnInit(): void {
+    this._setData();
+  }
+
+  tabChange(idName: string): void {
+    this.id = idName;
+  }
+
+  private _setData(): void {
+    this.showSpinner = true;
+    this._deviceService
+      .getHealthMeasuresMonthlySummary(this._deviceService.deviceRecordId!)
+      .pipe(finalize(() => (this.showSpinner = false)))
+      .subscribe({
+        next: (result) => {
+          result.forEach((element) => {
+            this.heartRateData.push(element.avgBpm);
+            this.oxygenSaturationData.push(element.avgSpo2);
+            this.dates.push(element.date);
+          });
+        },
+        error: (error) => {
+          this._snackBar.open('There are some problems', 'Close', {
+            duration: 3000,
+            horizontalPosition: 'right',
+            verticalPosition: 'top',
+          });
+        },
+      });
+  }
 }
